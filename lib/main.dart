da4f1e89b4c709045a806bb430e4c2039624f8e6
Your Name <you@example.com>
2023-07-31 21:48:41 +0900

    each page routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pachpach/screens/calendar.dart';
+import 'package:pachpach/screens/data.dart';
+import 'package:pachpach/screens/record.dart';
+import 'package:pachpach/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,152 +18,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => HomePage(),
+        '/record' : (context) => RecordPage(),
+        '/data' : (context) => DataPage(),
+        '/calendar' : (context) => CalendarPage(),
+      },
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget> [
-            Image.asset(
-                'assets/appLogo.png',
-              width: 100,
-              height: 100,
-            ),
-            IconButton(onPressed: (){}, icon: Icon(Icons.home_filled))
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: 300,
-              margin: EdgeInsets.only(top: 50, left: 10, bottom: 5),
-              child: const Text(
-                  '記録',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            Container(
-              width: 300,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Color(0xFF76FF03),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Container(
-                margin: EdgeInsets.all(20),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          Card(
-                            color: Colors.black54,
-                              child: Text(
-                                  '総合収支',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white
-                                ),
-                              ),
-                          ),
-                          Card(
-                            color: Colors.green,
-                              child: Text(
-                              '月間収支',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                            ),
-                          )),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const <Widget>[
-                          Text(
-                              '+2300000円',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                              '-1400円',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 70),
-              child: ElevatedButton.icon(
-                onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                  fixedSize: Size(200, 50),
-                  backgroundColor: Colors.black,
-                ),
-                label: Text("本日の記録"),
-                icon: const Icon(
-                    Icons.navigate_next,
-                  color: Color(0xFF76FF03),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 50),
-              child: ElevatedButton.icon(
-                onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                    fixedSize: Size(200, 50),
-                  backgroundColor: Colors.black,
-                ),
-                label: Text("カレンダー"),
-                icon: const Icon(
-                    Icons.navigate_next,
-                  color: Color(0xFF76FF03),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 50),
-              child: ElevatedButton.icon(
-                onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                    fixedSize: Size(200, 50),
-                  backgroundColor: Colors.black,
-                ),
-                label: Text("データ"),
-                icon: const Icon(
-                    Icons.navigate_next,
-                  color: Color(0xFF76FF03),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
---
+++ lib/screens/home.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget> [
+            Image.asset(
+              'assets/appLogo.png',
+              width: 100,
+              height: 100,
+            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.home_filled))
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 300,
+              margin: EdgeInsets.only(top: 50, left: 10, bottom: 5),
+              child: const Text(
+                '記録',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Color(0xFF76FF03),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Card(
+                            color: Colors.black54,
+                            child: Text(
+                              '総合収支',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white
+                              ),
+                            ),
+                          ),
+                          Card(
+                              color: Colors.green,
+                              child: Text(
+                                '月間収支',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Text(
+                            '+2300000円',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            '-1400円',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 70),
+              child: ElevatedButton.icon(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/record');
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(200, 50),
+                  backgroundColor: Colors.black,
+                ),
+                label: Text("本日の記録"),
+                icon: const Icon(
+                  Icons.navigate_next,
+                  color: Color(0xFF76FF03),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: ElevatedButton.icon(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/calendar');
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(200, 50),
+                  backgroundColor: Colors.black,
+                ),
+                label: Text("カレンダー"),
+                icon: const Icon(
+                  Icons.navigate_next,
+                  color: Color(0xFF76FF03),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: ElevatedButton.icon(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/data');
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(200, 50),
+                  backgroundColor: Colors.black,
+                ),
+                label: Text("データ"),
+                icon: const Icon(
+                  Icons.navigate_next,
+                  color: Color(0xFF76FF03),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
